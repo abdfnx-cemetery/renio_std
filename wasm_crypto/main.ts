b676687cb40028819e26cd79747967b2fd6fee5d
abdfnx <abdfnabd@gmail.com>
2021-11-25 07:47:38 +0300

    build the main file of wasm crypto, finish from `crypto` lib

---
+++ wasm_crypto/main.ts
@@ -0,0 +1,31 @@
+export * as crypto from "./crypto.js";
+
+export const digestAlgorithms = [
+  "BLAKE2B-256",
+  "BLAKE2B-384",
+  "BLAKE2B",
+  "BLAKE2S",
+  "BLAKE3",
+  "KECCAK-224",
+  "KECCAK-256",
+  "KECCAK-384",
+  "KECCAK-512",
+  "SHA-384",
+  "SHA3-224",
+  "SHA3-256",
+  "SHA3-384",
+  "SHA3-512",
+  "SHAKE128",
+  "SHAKE256",
+  // insecure (length-extendable):
+  "RIPEMD-160",
+  "SHA-224",
+  "SHA-256",
+  "SHA-512",
+  // insecure (collidable and length-extendable):
+  "MD5",
+  "SHA-1",
+] as const;
+
+/** An algorithm name supported by std/_wasm_crypto. */
+export type DigestAlgorithm = typeof digestAlgorithms[number];
